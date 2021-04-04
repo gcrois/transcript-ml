0ce7385c6bd84e66ab1dad426fb77b79b41551fb
Gregory Croisdale <gtcroisdale1@gmail.com>
2021-04-03 20:47:24 -0400

    yike

--- singleexec.py
+++ singleexec.py
@@ -22,12 +22,11 @@ data.to_csv(filename, header=True)
 
 print(f"Starting job #{job_n} on gpu #{gpu_n}")
 
-with tf.device(f'/GPU:{gpu_n}'):
-    try:
-        for j in range(len(jobs)):
-            print(f"Starting job #{j}\n")
-            resnet.ResNet(**jobs[j]).to_csv(filename, mode='a', header=False)
-            print(f"Done with job # {j}\n")
-    except:
-        print("interrupted! trying to save data")
-        sys.exit(0)
+try:
+    for j in range(len(jobs)):
+        print(f"Starting job #{j} of {len(jobs) - 1}\n")
+        resnet.ResNet(**jobs[j]).to_csv(filename, mode='a', header=False)
+        print(f"Done with job # {j} or {len(jobs) - 1}\n")
+except:
+    print("interrupted! trying to save data")
+    sys.exit(0)
