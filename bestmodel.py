36d63dfc988833c6435f0ade57c85c28a3f886be
Gregory Croisdale <gtcroisdale1@gmail.com>
2021-04-05 13:20:59 -0400

    bestmodel time

---
+++ bestmodel.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Residual CNN 
+# 
+# Tutorial used: https://adventuresinmachinelearning.com/introduction-resnet-tensorflow-2/
+
+
+from definitions import *
+from callback import *
+
+import os
+import numpy as np
+import cv2
+from PIL import Image
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+from tensorflow import keras
+from tensorflow.keras import layers
+import tensorflow as tf
+import datetime as dt
+import itertools
+import time
+from tensorflow.keras.callbacks import CSVLogger
+import pandas as pd
+
+import pickle
+
+#   Defines our 'block' of resnet   #
+#   Creates 2 blocks of resNet      #
+def res_net_block(input_data, filters, conv_size, Activation):
+  x = layers.Conv2D(filters, conv_size, activation=Activation, padding='same')(input_data)
+  x = layers.BatchNormalization()(x)
+  x = layers.Conv2D(filters, conv_size, activation=Activation, padding='same')(x)
+  x = layers.BatchNormalization()(x)
+  x = layers.Add()([x, input_data])
+  x = layers.Activation(Activation)(x)
+  return x
+
+
+def ResNet(HiddenLayers, LearningRate, Optimizer, NumFilters, Activation, KernelSize, Momentum, Epochs, BatchSize, JobNum):
+  # Load in all of our data #
+  X_train, X_test, y_train, y_test = pickle.load( open( "data/training_data.pickle", "rb" ) )
+
+  ############### Begin making the model ###############################
+
+  print(f"Creating the {HiddenLayers} deep blocks")
+  tic = time.time()
+
+  # Make the tensor with matching dimensions #
+  inputs = keras.Input(shape=(70, 70, CHANNELS))
+
+  x = layers.Conv2D(NumFilters, KernelSize, activation=Activation)(inputs)
+  x = layers.Conv2D(NumFilters, KernelSize, activation=Activation)(x)
+  x = layers.MaxPooling2D(3)(x)
+
+  #   Loop through making our blocks  #
+  for i in range(HiddenLayers // 2):
+      x = res_net_block(x, NumFilters, KernelSize, Activation)
+
+  # Pool, dense layer and ddropout  #
+  x = layers.GlobalAveragePooling2D()(x)
+  x = layers.Dense(256, activation= Activation)(x)
+  x = layers.Dropout(0.5)(x)
+
+  # Was originally softmax, might be for a good reason and should stay that way #
+  outputs = layers.Dense(24, activation="softmax")(x)
+
+  res_net_model = keras.Model(inputs, outputs)
+  print(f"{HiddenLayers} blocks done. Took:", round(time.time() - tic, 2), " seconds")
+
+  ############### Train ###############################
+  options = {opt: OPTMZ_ARGS[Optimizer][opt] for opt in OPTMZ_ARGS[Optimizer]}
+  for o in options:
+    options[o] = eval(options[o])
+
+  res_net_model.compile(optimizer=eval(f"keras.optimizers.{Optimizer}")(**options),
+                loss='sparse_categorical_crossentropy',
+                metrics=['acc'])
+
+  print(locals())
+  history = res_net_model.fit(x=X_train, y=y_train, batch_size=BatchSize, epochs=Epochs,
+      validation_data=(X_test, y_test), verbose=1)
+
+  data = pd.DataFrame(
+    columns=["HiddenLayers", "LearningRate", "Optimizer",
+            "NumFilters", "Activation", "KernelSize",
+            "Momentum", "Epochs", "Loss",
+            "Acc", "Val_Loss", "Val_Acc",]
+  )
+
+  for i in range(len(history.history["loss"])):
+    data.loc[len(data)] = [
+      HiddenLayers,
+      LearningRate,
+      Optimizer,
+      NumFilters,
+      Activation,
+      KernelSize,
+      Momentum,
+      i + 1, # epochs
+      history.history["loss"][i],
+      history.history["acc"][i],
+      history.history["val_loss"][i],
+      history.history["val_acc"][i],
+    ]
+
+  res_net_model.save("bestmodel")
+
+  return data
+
+ResNet(HiddenLayers=32, LearningRate=.001, Optimizer="RMSprop", NumFilters=96, Activation="relu", KernelSize=3, Momentum=.9, Epochs=50, BatchSize=512, JobNum=1)
