247ff4ba173477a8fd9b59e514d89f69d5c68f1a
Chase Chaser <thunte11@vols.utk.edu>
2021-08-19 14:08:36 -0400

    paper update

--- testLoad.py
+++ testLoad.py
@@ -1,6 +1,7 @@
 #   Using this to create the confusion matrix   #
 import tensorflow as tf
 from ModelCreation import *
+import numpy as np
 
 #   Import the model    #
 print("Creating the model")
@@ -10,14 +11,9 @@ model = ResNetModel(HiddenLayers=32, LearningRate=.001, Optimizer="RMSprop", Num
 print("Loading the weights")
 model.load_weights("training_1/cp.ckpt")
 
-#   Load the data   #
-print("Loading the data")
-X, Y = pickle.load(open("data/400k_training_data.pickle", "rb"))
+print(model.summary())
 
-#   Shorten it quickly  #
-X = X[:1000]
-Y = Y[:1000]
-
+"""
 print("Quick evaluation: ")
 print(model.evaluate(X, Y))
 
@@ -28,3 +24,4 @@ matrix = tf.math.confusion_matrix(Y, predictions, num_classes=24)
 
 print("Trying to print the matrix")
 print(matrix)
+"""
